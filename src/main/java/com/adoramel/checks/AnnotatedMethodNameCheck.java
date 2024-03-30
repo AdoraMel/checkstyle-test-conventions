ca10f9aaf6ad5f91eb3302faaa41a69ded62872a
AdoraMel <164820212+AdoraMel@users.noreply.github.com>
2024-03-31 00:26:41 +0100

    added javadoc

--- src/main/java/com/adoramel/checks/AnnotatedMethodNameCheck.java
+++ src/main/java/com/adoramel/checks/AnnotatedMethodNameCheck.java
@@ -1,8 +1,64 @@
 package com.adoramel.checks;
+/*
+ * Copyright (c) 2024-2024, the Checkstyle Test Conventions cotributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 import com.puppycrawl.tools.checkstyle.api.*;
 
 import java.util.Arrays;
  
+/**
+ * <p>
+ * Checks that methods with certain annotations are named to conform to a 
+ * specified pattern.
+ * </p>
+ * 
+ * <p>
+ * By default checks that methods annotated with {@code @Test} start with "test":
+ * </p>
+ * <pre>
+    @Test
+    public void additionTest() { //violation
+        MyClass example = new MyClass();
+        assertEquals(8, example.add(5, 3));
+    }
+    <br>
+    @Test
+    public void testSubtract() { //ok, starts with "test"
+        MyClass example = new MyClass();
+        assertEquals(5, example.subtract(8, 3));
+    }
+ * </pre>
+ * <br>
+ * <ul>
+ * <li>
+ * Property {@code annotations} - Sets the annotations that method names wil
+ * be checked for.
+ * Type is {@code String[]}.
+     * Default value is {@code ["@Test"]}.
+ * </li>
+ * <li>
+ * Property {@code methodPattern} - Sets the pattern to match valid method names.
+ * Type is {@code java.util.regex.Pattern}.
+ * Default value is {@code "^test.*$"}.
+ * </li>
+ * </ul>
+ * <p>
+ * Parent is {@code com.puppycrawl.tools.checkstyle.TreeWalker}
+ * </p>
+ */
 public class AnnotatedMethodNameCheck extends AbstractCheck
 {
   private static final String[] _DEFAULT_ANNOTATIONS = { "@Test" };
@@ -36,13 +92,24 @@ public class AnnotatedMethodNameCheck extends AbstractCheck
     }
   }
 
-  //set annotations
+  /**
+     * Setter.
+     *
+     * @param annotations Sets the annotations that method names wil
+     * be checked for.<br>
+     * Default value is {@code ["@Test"]}.
+     */
   public void setAnnotations(String[] annotations)
   {
     _annotations = annotations;
   }
 
-  //set method pattern
+  /**
+     * Setter.
+     *
+     * @param methodPattern Sets the pattern to match valid method names.<br>
+     * <br>Default value is {@code "^test.*$"}.
+     */
   public void setMethodPattern(String methodPattern)
   {
     _methodPattern = methodPattern;
--- src/main/java/com/adoramel/checks/TestMethodCommentsCheck.java
+++ src/main/java/com/adoramel/checks/TestMethodCommentsCheck.java
@@ -1,8 +1,70 @@
 package com.adoramel.checks;
+/*
+ * Copyright (c) 2024-2024, the Checkstyle Test Conventions cotributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 import com.puppycrawl.tools.checkstyle.api.*;
 
 import java.util.Arrays;
  
+/**
+ * <p>
+ * Checks that methods with certain annotations have comments that adhere to a
+ * specified pattern. The pattern applies to all comments concatenated together.
+ * </p>
+ * 
+ * <p>
+ * By default checks that methods annotated with {@code @Test} have comments where
+ * "given", "when", "then" appear in order:
+ * </p>
+ * <pre>
+    @Test
+    public void additionTest() { //ok
+        //given
+        MyClass example = new MyClass();
+        //when/then
+        assertEquals(8, example.add(5, 3));
+    }
+    <br>
+    @Test
+    public void testSubtract() { //violation, "when" missing
+        //given a new class
+        MyClass example = new MyClass();
+        //then
+        assertEquals(5, example.subtract(8, 3));
+    }
+ * </pre>
+ * <br>
+ * <ul>
+ * <li>
+ * Property {@code annotations} - Sets the annotations that method comments wil
+ * be checked for.
+ * Type is {@code String[]}.
+ * Default value is {@code @Test}.
+ * </li>
+ * <li>
+ * Property {@code commentPattern} - Sets the pattern to match valid comments.<br>
+ * <b>NOTE</b>: All comments are concatenated when pattern is checked.
+ * Type is {@code java.util.regex.Pattern}.
+ * Default value is {@code "(?s)^.*\\bgiven\\b.*\\bwhen\\b.*\\bthen\\b.*$"}.
+ * </li>
+ * </ul>
+ * <p>
+ * Parent is {@code com.puppycrawl.tools.checkstyle.TreeWalker}
+ * </p>
+ */
 public class TestMethodCommentsCheck extends AbstractCheck
 {
   private static final String[] _DEFAULT_ANNOTATIONS = { "@Test" };
@@ -68,13 +130,25 @@ public class TestMethodCommentsCheck extends AbstractCheck
     }
   }
 
-  //set annotations
+  /**
+     * Setter.
+     *
+     * @param annotations Sets the annotations that method comments wil
+     * be checked for.<br>
+     * Default value is {@code ["@Test"]}.
+     */
   public void setAnnotations(String[] annotations)
   {
     _annotations = annotations;
   }
 
-  //set comments pattern
+  /**
+     * Setter.
+     *
+     * @param commentsPattern  Sets the pattern to match valid comments.<br>
+     * <br><b>NOTE</b>: All comments are concatenated when pattern is checked. <br>
+     * <br>Default value is {@code "(?s)^.*\\bgiven\\b.*\\bwhen\\b.*\\bthen\\b.*$"}.
+     */
   public void setCommentsPattern(String commentsPattern)
   {
     _commentsPattern = commentsPattern;
